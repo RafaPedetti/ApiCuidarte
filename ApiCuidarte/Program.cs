2d5d1f4c239c50a80011c8c611910ac8b9d86c8e
RafaPedetti <rafapedetti@gmail.com>
2026-01-22 16:12:09 -0300

    cambio para que suba las tablas

--- ApiCuidarte/Program.cs
+++ ApiCuidarte/Program.cs
@@ -214,6 +214,13 @@ namespace ApiCuidarte
 			);
 			var app = builder.Build();
 
+			using (var scope = app.Services.CreateScope())
+			{
+				var db = scope.ServiceProvider.GetRequiredService<CuidarteContext>();
+				db.Database.Migrate();
+			}
+
+
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{

3cc0a4ad31ad9c46d99a90acbf5afbc4fc69d6ae
RafaPedetti <rafapedetti@gmail.com>
2026-01-22 22:22:18 -0300

    cambio url porque cambio(?

--- ApiCuidarte/Program.cs
+++ ApiCuidarte/Program.cs
@@ -49,7 +49,7 @@ namespace ApiCuidarte
 				{
 					policy.WithOrigins(
 						"http://localhost:3000",
-						"https://cuidarte-frontend-m5s94vpmu-rafapedettis-projects.vercel.app"
+						"https://cuidarte-frontend-iefli298f-rafapedettis-projects.vercel.app"
 					)
 					.AllowAnyHeader()
 					.AllowAnyMethod();

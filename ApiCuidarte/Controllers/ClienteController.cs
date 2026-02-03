814cac36570171f4942901ed511826f81e4f0679
RafaPedetti <rafapedetti@gmail.com>
2026-02-03 20:53:11 -0300

    crearVarios

--- ApiCuidarte/Controllers/ClienteController.cs
+++ ApiCuidarte/Controllers/ClienteController.cs
@@ -53,7 +53,41 @@ namespace ApiCuidarte.Controllers
 		{
 			try
 			{
-				 _alta.Ejecutar(cliente);
+				ClienteDto cCreado = _alta.Ejecutar(cliente);
+				return Ok(cCreado);
+			}
+			catch (DomainException ex)
+			{
+				return BadRequest(new { message = ex.Message });
+			}
+			catch (UnauthorizedAccessException)
+			{
+				return Unauthorized(new { message = "No autorizado" });
+			}
+			catch (NotFoundException ex)
+			{
+				return NotFound(new { message = ex.Message });
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError,
+					new { message = "Hupps hubo un error, intente nuevamente más tarde" });
+			}
+
+		}
+
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+		[HttpPost]
+		[Route("CrearVarios")]
+		public IActionResult CrearVarios(IEnumerable<ClienteDto> clientes)
+		{
+			try
+			{
+				foreach (var cliente in clientes)
+				{
+					_alta.Ejecutar(cliente);
+				}
 				return Ok();
 			}
 			catch (DomainException ex)
